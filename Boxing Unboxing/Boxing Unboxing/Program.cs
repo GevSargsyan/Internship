b626511b7d6e096c3b2cae489da768f4c321aeb8
Gev <Gev@DESKTOP-FN6RES1>
2021-06-10 15:43:13 +0400

    Boxing and Unboxing

---
+++ Boxing Unboxing/Boxing Unboxing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Boxing_Unboxing
+{
+    struct Point
+    {
+        public Int32 x, y;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = new ArrayList();
+            Point p; // Memory allocated for Point(not on the heap)
+            for (Int32 i = 0; i < 10; i++)
+            {
+                p.x = p.y = i; // Initializing members in our value type
+                a.Add(p); //  Significant type packing and adding
+                          // references in ArrayList
+
+            }
+        }
+    }
+}
